94aef34f538f7e64e0d908b55665f722c7f44517
francisco8628 <francisco.ableite@gmail.com>
2020-05-14 21:55:11 -0300

    inserir Clases aluno e executavel

---
+++ src/Classes/Aluno.java
@@ -0,0 +1,36 @@
+package Classes;
+
+public class Aluno {
+	
+	  private String nome;
+
+      private int idade;
+
+      private String Materia;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getMateria() {
+		return Materia;
+	}
+
+	public void setMateria(String materia) {
+		Materia = materia;
+	}
+
+      
+}
---
+++ src/executavel/ClasseExecutavel.java
@@ -0,0 +1,15 @@
+package executavel;
+
+import Classes.Aluno;
+
+public class ClasseExecutavel {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		 aluno.setNome("francisco");
+		
+		System.out.println(aluno.getNome());
+	}
+
+}
