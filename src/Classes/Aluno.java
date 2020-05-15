b508c6153814a3b8c4b3445a1da2abd710876898
francisco8628 <francisco.ableite@gmail.com>
2020-05-14 22:15:42 -0300

    calculo das 3 notas

--- src/Classes/Aluno.java
+++ src/Classes/Aluno.java
@@ -7,6 +7,36 @@ public class Aluno {
       private int idade;
 
       private String Materia;
+      
+      private double nota1;      
+      private double nota2;      
+      private double nota3;
+      
+      
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
 
 	public String getNome() {
 		return nome;
@@ -32,5 +62,10 @@ public class Aluno {
 		Materia = materia;
 	}
 
+	 
+	public double getMediaNota() {
+		
+		return (nota1+nota2+nota3)/3;
+	}
       
 }
--- src/executavel/ClasseExecutavel.java
+++ src/executavel/ClasseExecutavel.java
@@ -8,8 +8,11 @@ public class ClasseExecutavel {
 		
 		Aluno aluno = new Aluno();
 		 aluno.setNome("francisco");
-		
+		 aluno.setNota1(90);
+		 aluno.setNota2(70);
+		 aluno.setNota3(85);
 		System.out.println(aluno.getNome());
+		System.out.println(aluno.getMediaNota());
 	}
 
 }
